d97c8bbbf8b7667b84d60b66f3490a311bc74b12
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-11-04 19:39:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+function jumpDodger() {
+  let bottomNumbers = dodger.style.bottom.replace("px", "");
+  let bottom = parseInt(bottomNumbers, 10);
+ 
+  if (bottom >= 0) {
+    dodger.style.bottom = `${bottom + 2}px`;
+  }
+  if (bottom > 380) {
+	dodger.style.bottom = '380px'
+  }
+  
+}
+
+function downDodger() {
+  let bottomNumbers = dodger.style.bottom.replace("px", "");
+  let bottom = parseInt(bottomNumbers, 10);
+ 
+  if (bottom > 0) {
+    dodger.style.bottom = `${bottom - 3}px`;
+  }
+  if (bottom < 0 ) {
+	dodger.style.bottom =  '0px'
+  }
+}
